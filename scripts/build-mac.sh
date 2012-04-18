e60d98242782c5723b15b88c048b4921c46743cd
Jordon Mears <jordoncm@gmail.com>
2012-04-17 21:09:33 -0400

    Adding the basic build scripts.

---
+++ scripts/build-mac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd `dirname $0`/..
+python setup.py py2app --iconfile=icons/icon.icns
---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd `dirname $0`/..
+
+VERSION=$1
+
+rm -rf build
+rm -rf dist
+
+mkdir build
+mkdir build/cider-$VERSION
+
+cp -r static build/cider-$VERSION/
+cp -r templates build/cider-$VERSION/
+cp -r tornado build/cider-$VERSION/
+cp configuration.json build/cider-$VERSION/
+cp license.txt build/cider-$VERSION/
+cp readme.txt build/cider-$VERSION/
+cp server.py build/cider-$VERSION/
+
+cd build
+tar -czf cider-$VERSION.tar.gz cider-$VERSION
+
+cd ..
+mkdir dist
+cp build/cider-$VERSION.tar.gz dist/
