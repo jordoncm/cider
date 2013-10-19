8ecffb2c82f4cbf50a9fb26e16b39d2d445862fa
Jordon Mears <jordoncm@gmail.com>
2013-10-19 15:20:05 -0400

    Another try for openshift.

--- openshift.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-cwd = os.path.dirname(os.path.abspath(__file__))
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(cwd, '..', 'misc/virtenv/lib/python2.7/site-packages')
-virtualenv = os.path.join(cwd, '..', 'misc/virtenv/bin/activate_this.py')
-execfile(virtualenv, dict(__file__=virtualenv))
-
-import server
-server.main()
